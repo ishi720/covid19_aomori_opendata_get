1905da4464e6126fb1515184ce48f11faf55d517
ishi720 <ishizuka720@gmail.com>
2020-04-03 18:43:10 +0900

    ロジック修正

--- csv_to_json.js
+++ csv_to_json.js
@@ -3,23 +3,23 @@ const fs = require('fs');
 const iconv = require('iconv-lite');
 const csv = require('convert-csv-to-json');
 
-csv_to_json('./csv/consult.csv','./json/consult.json');
-csv_to_json('./csv/consult_call_center.csv','./json/consult_call_center.json');
-csv_to_json('./csv/inspect.csv','./json/inspection.json');
+csv_to_json('consult.csv','consult.json');
+csv_to_json('consult_call_center.csv','consult_call_center.json');
+csv_to_json('inspect.csv','inspection.json');
 
 function csv_to_json (inputFile, outputFile) {
 	genCSV().then(res => {
-	    const fileInputName = 'temp.csv'; 
-	    const fileOutputName = outputFile;
+	    const fileInputName = 'temp_' + inputFile; 
+	    const fileOutputName = './json/'+ outputFile;
 	    csv.fieldDelimiter(',') .getJsonFromCsv(fileInputName);
 	    csv.generateJsonFileFromCsv(fileInputName,fileOutputName);
 	});
 	function genCSV(){
 	    return new Promise((resolve, reject) => {
-	        fs.readFile(inputFile, function(err, data){
+	        fs.readFile('./csv/'+ inputFile, function(err, data){
 	            const buf = new Buffer.from(data, 'binary');
 	            const retStr = iconv.decode(buf, "Shift_JIS");
-	            fs.writeFile('temp.csv', retStr, (err) => {
+	            fs.writeFile('temp_' + inputFile, retStr, (err) => {
 	                if (err) throw err;
 	                resolve(retStr);  
 	            });
