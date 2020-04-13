6e565d375b24bd10dfd5fe08b07f7e1239773652
ishi720 <ishizuka720@gmail.com>
2020-04-13 15:12:27 +0900

    青森データカタログの更新日時も反映させるためのjsonデータを作成する処理を追加

--- csv_get.js
+++ csv_get.js
@@ -1,5 +1,6 @@
 'use strict';
 
+const fs = require("fs");
 const request = require("request");
 const wget = require('node-wget');
 const url = 'https://opendata.pref.aomori.lg.jp/api/package_show?id=5e4612ce-1636-41d9-82a3-c5130a79ffe0';
@@ -10,6 +11,19 @@ request.get({ url: url }, function (error, response, body) {
 
     resources.forEach( (val) => {
         if (val.name.match(/相談件数（コールセンター）/)) {
+
+
+            // 更新日時のjson作成
+            const consult_call_center_last_update= {
+                "date": val.updated.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/,"$1/$2/$3")
+            }
+
+            fs.writeFile('json/consult_call_center.json', JSON.stringify(consult_call_center_last_update, null, 4), (err, data) => {
+              if(err) console.log(err);
+              else console.log('save_consult_call_center.json');
+            });
+
+            // csv作成
             wget({
                 url: val.url,
                 dest: './csv/consult_call_center.csv'
@@ -21,6 +35,18 @@ request.get({ url: url }, function (error, response, body) {
                 }
             });
         } else if (val.name.match(/相談件数（帰国者・接触者相談）/)) {
+
+            // 更新日時のjson作成
+            const consult_last_update = {
+                "date": val.updated.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/,"$1/$2/$3")
+            }
+
+            fs.writeFile('json/consult.json', JSON.stringify(consult_last_update, null, 4), (err, data) => {
+              if(err) console.log(err);
+              else console.log('consult.json');
+            });
+
+            // csv作成
             wget({
                 url: val.url,
                 dest: './csv/consult.csv'
@@ -32,6 +58,18 @@ request.get({ url: url }, function (error, response, body) {
                 }
             });
         } else if (val.name.match(/検査実施状況/)) {
+
+            // 更新日時のjson作成
+            const inspect_last_update = {
+                "date": val.updated.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/,"$1/$2/$3")
+            }
+
+            fs.writeFile('json/inspect.json', JSON.stringify(inspect_last_update, null, 4), (err, data) => {
+              if(err) console.log(err);
+              else console.log('inspect.json');
+            });
+
+            // csv作成
             wget({
                 url: val.url,
                 dest: './csv/inspect.csv'
@@ -43,6 +81,18 @@ request.get({ url: url }, function (error, response, body) {
                 }
             });
         } else if (val.name.match(/陽性患者関係/)) {
+
+            // 更新日時のjson作成
+            const patients_last_update = {
+                "date": val.updated.replace(/^(\d{4})-(\d{2})-(\d{2}).+$/,"$1/$2/$3")
+            }
+
+            fs.writeFile('json/patients.json', JSON.stringify(patients_last_update, null, 4), (err, data) => {
+              if(err) console.log(err);
+              else console.log('patients.json');
+            });
+
+            // csv作成
             wget({
                 url: val.url,
                 dest: './csv/patients.csv'
@@ -53,6 +103,7 @@ request.get({ url: url }, function (error, response, body) {
                     console.log("Save to patients.csv");
                 }
             });
+
         } else if (val.name.match(/検査陽性者の状況/)) {
         }
     })
