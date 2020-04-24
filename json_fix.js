710ce079dc174e561d935546a6dd9a1a4cd7b828
ishi720 <ishizuka720@gmail.com>
2020-04-24 09:04:16 +0900

    インデント修正

--- json_fix.js
+++ json_fix.js
@@ -6,13 +6,13 @@ const fs = require('fs');
 const jsonObject = JSON.parse(fs.readFileSync('./json/_consult_call_center.json', 'utf8'));
 const workObj = [];
 Object.keys(jsonObject).forEach(function (key) {
-  if( jsonObject[key]['全国地方公共団体コード'].match(/※/)) {
-  } else {
-    workObj.push(jsonObject[key]);
-  }
+    if( jsonObject[key]['全国地方公共団体コード'].match(/※/)) {
+    } else {
+        workObj.push(jsonObject[key]);
+    }
 });
 
 fs.writeFile('./json/_consult_call_center.json', JSON.stringify(workObj, null, 2), (err, data) => {
-  if(err) console.log(err);
-  else console.log('_consult_call_center.json fix');
+    if(err) console.log(err);
+    else console.log('_consult_call_center.json fix');
 });
