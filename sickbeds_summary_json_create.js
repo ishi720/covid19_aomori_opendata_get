d88cc426bb00cc1f000e883ba87a559308c7bd8f
ishi720 <ishizuka720@gmail.com>
2020-10-19 11:56:36 +0900

    病床数を取得するAPIの変更準備

--- sickbeds_summary_json_create.js
+++ sickbeds_summary_json_create.js
@@ -36,7 +36,46 @@ request('https://www.stopcovid19.jp/data/covid19japan.json')
             .catch(error => {
                 throw error;
             });
+
+        //病床数の取得
+        // request('https://www.stopcovid19.jp/data/covid19japan_beds/latest.json')
+        //     .then(body => {
+        //         const beds = JSON.parse(body);
+
+        //         // 出力するデータの作成
+        //         const outputData = {
+        //             'sickbeds_summary': {
+        //                 'data': {
+        //                     '入院患者数': ncurrentpatients,
+        //                     '残り病床数':  Number(beds[1]['入院患者受入確保病床']) + Number(beds[1]['宿泊施設受入可能室数']) - ncurrentpatients
+        //                 },
+        //                 'last_update': getNewerDate(beds[1]['更新日'].replace(/-/g,'/'), ncurrentpatients_lastUpdate.replace(/-/g,'/'))
+        //             }
+        //         };
+        //         fs.writeFile('json/sickbeds_summary.json', JSON.stringify(outputData, null, 4), (err, data) => {
+        //             if(err) console.log(err);
+        //             else console.log('sickbeds_summary.json write');
+        //         });
+        //     })
+        //     .catch(error => {
+        //         throw error;
+        //     });
+
     })
     .catch(error => {
         throw error;
     });
+
+
+/**
+ * 新しいほうの日付を取得する
+ * @param {string} d1 - 日付1
+ * @param {string} d2 - 日付2
+ */
+let getNewerDate = (d1, d2) => {
+    if ( new Date(d1) > new Date(d2)) {
+        return d1;
+    } else {
+        return d2;
+    }
+}
